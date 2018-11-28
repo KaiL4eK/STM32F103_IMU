daa6327fcc59cd44bf7dc6fe1df354516e9c98e7
KaiL4eK <darklexi494@gmail.com>
2018-11-29 02:43:50 +0300

    Non working I2C

---
+++ adxl345.c
@@ -0,0 +1,27 @@
+#include <i2c_interface.h>
+
+i2c_module_t m_i2c = NULL;
+
+#define ADXL345_ADDR    0x53
+
+int adxl345_init( i2c_module_t p_i2c )
+{
+    m_i2c = p_i2c;
+
+    return EOK;
+}
+
+uint8_t adxl345_get_self_id( void )
+{
+    if ( !m_i2c )
+        return 0;
+
+    uint8_t result = i2c_read_byte( m_i2c, ADXL345_ADDR, 0x00 );
+
+    if ( i2c_get_errno( m_i2c ) != EOK )
+    {
+        return 0;
+    }
+
+    return result;
+}
